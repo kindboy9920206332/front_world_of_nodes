7997bd13a445fe77d90cf10e53c16662da67f089
mohammadreza <kindboy2025@gmail.com>
2025-12-29 14:34:38 +0330

    start project

---
+++ app/home.tsx
@@ -0,0 +1,9 @@
+export default function HomeGame () {
+    return (
+        <>
+        
+        <div></div>
+        
+        </>
+    )
+}
---
+++ utils/fetching.ts
@@ -0,0 +1,48 @@
+
+
+export type GET = {
+    url:string
+}
+
+
+export type POUA ={
+      url:string
+      request:any  
+}
+
+
+
+export class apiClient {
+
+get = async(url:string)=>{
+
+
+
+}
+
+
+post = async({request,url}:POUA)=>{
+
+}
+
+
+
+put = async({request,url}:POUA)=>{
+
+}
+
+
+
+
+patch = async({request,url}:POUA)=>{
+
+}
+
+delete = async(url:string)=>{
+
+}
+
+
+
+
+}
